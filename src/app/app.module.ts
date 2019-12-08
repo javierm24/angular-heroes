17ad1904dfa3c90ef3c46e533896112ab5963b42
Javier Machado <javierm1694@gmail.com>
2019-12-08 23:21:24 +0100

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
+// Rutas
+import { APP_ROUTING } from './app.routes';
+
+// Servicios
+
+
+// Componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
@@ -18,6 +25,7 @@ import { HeroesComponent } from './components/heroes/heroes.component';
   ],
   imports: [
     BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
